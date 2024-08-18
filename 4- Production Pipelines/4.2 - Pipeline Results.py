98f2f2bf1031ea9187ca91236d2950c445bea02a
sotumu <83347871+sotumu@users.noreply.github.com>
2024-08-18 15:54:41 +0200

    Sotumu notes

--- 4- Production Pipelines/4.2 - Pipeline Results.py
+++ 4- Production Pipelines/4.2 - Pipeline Results.py
@@ -2,10 +2,19 @@
 files = dbutils.fs.ls("dbfs:/mnt/demo/dlt/demo_bookstore")
 display(files)
 
+SOTUMU NOTES: the output shows 
+1. autoloader
+2. checkpoints
+3. system
+4. tables
+
+
 # COMMAND ----------
 
 files = dbutils.fs.ls("dbfs:/mnt/demo/dlt/demo_bookstore/system/events")
 display(files)
+
+ALL UI events of the DLT
 
 # COMMAND ----------
 
